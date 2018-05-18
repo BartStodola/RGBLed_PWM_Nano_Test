3f91378c7de439f28f31f1cd4d12a948537ad565
chris.stadler <stadler.christian@web.de>
2018-05-18 22:38:12 +0200

    new version based in counter and HW interrupt

--- src/main.ino
+++ src/main.ino
@@ -1,117 +1,114 @@
-#include <Arduino.h>
+enum States
+{
+  IsIdle, FadeIn, TurnY2R, FadeOut
+} state;
+int fadeInCount=0;
+int fadeOutCount=0;
+int y2RCount=0;
+int isReset = true;
+
+// pins
+const int potiPin = 1;
+const int inputPin = 2;
+const int triggerPin =3;//LED
 
 // Init the Pins used for PWM arduino nano
 const int redPin = 11;
 const int greenPin = 10;
 const int bluePin = 9;
 
-// for ATTiny85
-// on button pressed fade led in stay for n-sec. and fade out
+const int fadeInDelayTime = 10;
+const int fadeOutDelayTime = 20;
 
-//int brightness[] = { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 6, 7, 8, 9, 10, 11, 13, 14, 16, 18, 21, 23, 26, 30, 33, 37, 42, 47, 53, 59, 66, 74, 83, 93, 104, 116, 130, 145, 162, 181, 203, 227, 255 };
-int brightness[] = { 0, 0, 0, 0, 0, 1, 1, 2, 2, 3, 4, 5, 6, 7, 9, 11, 13, 15, 18, 22, 26, 31, 37, 44, 53, 62, 74, 87, 104, 123, 145, 172, 203, 240, 255 };
-//int brightness[] = { 0, 0, 0, 0, 1, 2, 2, 3, 4, 6, 8, 10, 13, 16, 21, 26, 33, 42, 53, 66, 83, 104, 130, 162, 203, 255 };
-int count = (sizeof(brightness)/sizeof(*brightness));
-
-const int triggerPin =3;//LED
-//const int ledPin = 1;  // LED
-const int inputPin = 2;// button or PIR-Sensor
-const int potiPin = 4;
-
-int processing = -1;
-int maxProcessingCount = 10;
-
-void FadeIn(int delayMsec)
-{
-  for (int i= 0; i<255;++i)
-  {
-    analogWrite(redPin, i);
-    analogWrite(greenPin, i);
-    delay(delayMsec);
-  }
-}
-
-// fade from red to black.
-void FadeOut(int delayMsec)
-{
-  for (int i= 0; i<255;++i)
-  {
-    analogWrite(redPin, 255-i);
-    analogWrite(greenPin, 0);
-    delay(delayMsec);
-  }
-  analogWrite(redPin, 0);
-  analogWrite(greenPin, 0);
-}
-
-boolean IsTriggerSet()
+enum States TriggerSet()
 {
   if (digitalRead(inputPin) == HIGH) {
-    // turn LED on:
-    return true;
+    //-- motion detected.
+
+    if (state == IsIdle)
+    return FadeIn;
+    else if (state != FadeIn) {ResetVariables(); return TurnY2R;}
   } else {
     // turn LED off:
-    return false;
+   return state;
   }
 }
-
-void BlinkTriggerLed(int msec)
+void ResetVariables()
 {
-  digitalWrite(triggerPin, HIGH);
-  delay(msec);
-  digitalWrite(triggerPin, LOW);
+   fadeInCount=0;
+   fadeOutCount=0;
+   y2RCount=0;
 }
 
 void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("setup");
 
-  pinMode(inputPin, INPUT);
-  pinMode(potiPin, INPUT);
+  state = IsIdle;
+  ResetVariables();
 
   pinMode(triggerPin, OUTPUT);
-  pinMode(redPin, OUTPUT);
-  pinMode(greenPin, OUTPUT);
-  pinMode(bluePin, OUTPUT);
-  processing = -1;
+  pinMode(inputPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(inputPin), blink, HIGH);
+}
+
+void blink() {
+  digitalWrite(triggerPin, HIGH);
+  delay(500);
+  digitalWrite(triggerPin, LOW);
 }
 
 void loop() {
   analogWrite(bluePin, 0);
-  maxProcessingCount = map(analogRead(potiPin), 0,1023, 0,200);
+  int fadeY2RDelayTime = map(analogRead(potiPin), 0,1023, 0,200);
 
-  if (IsTriggerSet())
+  int potiVal = analogRead(potiPin);
+  state = TriggerSet();
+
+  if (state == FadeIn)
   {
-    if (processing == -1)
-    {
-      BlinkTriggerLed(500);
+    isReset = false;
+    // fade in..
+    analogWrite(redPin, fadeInCount);
+    analogWrite(greenPin, fadeInCount);
+    delay(fadeInDelayTime);
 
-      /// --- fade in
-      ///
-      processing = maxProcessingCount;
-      FadeIn(10);
-    }
-    if (processing > -1)
-    {
-      // set re-trigger count
-      BlinkTriggerLed(500);
-      processing = maxProcessingCount;
-    }
+    if (fadeInCount++ == 255) state = TurnY2R;
   }
 
-  if (processing > 0)
+  if (state == TurnY2R)
   {
-    /// --- illuminate (slowly fade from yellow to red)
-    ///
+    // turn color yellow to red.
     analogWrite(redPin, 255);
-    analogWrite(greenPin, 255-255/(processing+1));
-    delay(200);
+    analogWrite(greenPin, 255 - y2RCount);
+    delay(fadeY2RDelayTime);
 
-    processing -= 1;
+    if (y2RCount++ == 255) state = FadeOut;
   }
-  if (processing == 0)
+
+  if (state == FadeOut)
   {
-    /// --- fade out
-    ///
-    FadeOut(10);
-    processing =-1;
+    // fade in..
+    analogWrite(redPin, 255 - fadeOutCount);
+    analogWrite(greenPin, 0);
+    delay(fadeOutDelayTime);
+
+    if (fadeOutCount++ == 255) state = IsIdle;
   }
+
+  if (state == IsIdle && !isReset)
+  {
+    isReset = true;
+    ResetVariables();
+  }
+
+  // report status
+//  Serial.print(state);
+//  Serial.print(" -->> ");
+//  Serial.print(fadeInCount);
+//  Serial.print(", ");
+//  Serial.print(y2RCount);
+//  Serial.print(", ");
+//  Serial.println(fadeOutCount);
 }
